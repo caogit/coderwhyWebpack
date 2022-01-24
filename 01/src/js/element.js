b048b85bfc47d63030651c230bd6e0500284b5c8
caoao <chou45169@163.com>
2022-01-24 17:03:27 +0800

    插件的使用，打包开发或生产，source-map

--- 01/src/js/element.js
+++ 01/src/js/element.js
@@ -1,11 +1,31 @@
 import '../css/style.css'
 import '../css/style.less'
+import '../css/bgImage.css'
 import '../css/testpostcss.css'
+import '../font/iconfont.css'
+
+import zznhImage from '../img/zznh.png'
 
 const div = document.createElement('div')
 div.className = 'title'
 div.innerHTML ='哈哈哈'
 
+// 设置背景图片
+const imgdiv = document.createElement('div')
+imgdiv.className = 'image-bg'
+
+const  imgEl = document.createElement('img')
+imgEl.src = zznhImage
+
+// 添加iconfont
+const icon = document.createElement('i')
+icon.className = 'iconfont icon-ashbin'
+
+console.log(foo.data)
+
 document.body.appendChild(div)
+document.body.appendChild(imgdiv)
+document.body.appendChild(imgEl)
+document.body.appendChild(icon)
 
  
--- 01/webpack.config.js
+++ 01/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {DefinePlugin} = require('webpack')
+const CopyPlugin  = require('copy-webpack-plugin')
 
 module.exports = {
+    mode:'development',
+    devtool:'source-map',
     entry:'./src/main.js',
     output:{
         path:path.resolve(__dirname,'./build'),
-        filename:'build.js'
+        filename:'js/build.js',
+        // assetModuleFilename: 'images/[hash][ext][query]'
     },
     module:{
         rules:[
@@ -25,6 +32,69 @@ module.exports = {
                 "less-loader"
                 ]
             },
+            // {
+            //     test:/\.(png|jpg)$/,
+            //     use:[{
+            //         loader:"url-loader",
+            //         options:{
+            //             name:'img/[name]_[hash:6].[ext]',
+            //             esModule:false,
+            //             limit:100*1024
+
+            //         }
+            //     }],
+            //     type: 'javascript/auto'
+            // },
+            {
+                test:/\.(jpg|png)$/,
+                type:'asset',
+                parser:{
+                    dataUrlCondition:{
+                        maxSize: 10  * 1024
+                    }
+                },
+                generator: {
+                    filename: 'img/[name]_[hash:6][ext]'
+                }
+            },
+            // {
+            //     test:/\.(eot|ttf|woff|woff2)$/,
+            //     loader:'file-loader',
+            //     options:{
+            //         name:'font/[name]_[hash:6].[ext]',
+            //             esModule:false,
+            //     },
+            //     type: 'javascript/auto'
+
+            // }
+            {
+                test:/\.(eot|ttf|woff|woff2)$/,
+                type:'asset/resource',
+                generator:{
+                    filename:'font/[name]_[hash:6][ext]'
+                }
+            }
         ]
-    }
-}
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            title:'啊哈哈哈'
+        }),
+        new DefinePlugin({
+            BASE_URL:"'./'"
+        }),
+        new CopyPlugin({
+            patterns:[
+                {
+                    from:'./public',
+                    to:'./',
+                    globOptions:{
+                        ignore:['**/index.html']
+                    }
+                }
+            ]
+        })
+    ]
+} 
