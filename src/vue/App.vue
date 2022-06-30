7116eb80e56d38648af20d163a8cad23eb2e5dbc
caoao <chou45169@163.com>
2022-06-30 10:58:10 +0800

    feat:增加移动端使用postcss-px-to-viewport进行适配

---
+++ src/vue/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">{{ title }}</div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      title: '哈哈Vue12',
+    }
+  },
+}
+</script>
+<style></style>
